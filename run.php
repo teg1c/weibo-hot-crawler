5290159f53755820ab61f38a9251d02a7781bfcc
tegic <teg1c@foxmail.com>
2021-10-08 16:41:38 +0800

    update

--- run.php
+++ run.php
@@ -17,11 +17,16 @@ $readMeHead = <<< Eof
 ## 微博今日热榜 更新于 {$time} \n
 Eof;
 $client = new Client([
-    'timeout' => 10
+    'timeout' => 10,
+    'allow_redirects' => true,
 ]);
-$response = $client->get($url);
+$response = $client->get($url, [
+        'headers'=>[
+                'User-Agent'=>'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+            'Cookie'=>'SUB=_2AkMWPIzuf8NxqwJRmPgSxGrlZI1xwwvEieKgYH01JRMxHRl-yT8Xqn0HtRB6PbyiAY7W0wZkwFc1nXHJxUddZr9bpaPQ; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9W5p6Ee5xHahOckML_sA0l2c; _s_tentry=passport.weibo.com; Apache=6467906874370.302.1633682394000; SINAGLOBAL=6467906874370.302.1633682394000; ULV=1633682394006:1:1:1:6467906874370.302.1633682394000:; WBStorage=6ff1c79b|undefined'
+        ]
+]);
 $html = $response->getBody()->getContents();
-
 $table = QueryList::html($html)->find('table:eq(0)');
 $result = $table->find('tr:gt(1)')->map(function ($row) use (&$readMeHead) {
     $regular = '@<a href="(\/weibo\?q=[^"]+)".*?>(.+)<\/a>@';
@@ -36,5 +41,4 @@ $result = $table->find('tr:gt(1)')->map(function ($row) use (&$readMeHead) {
     ];
 })->filter()->values();
 $content = $result->all();
-file_put_contents("README.MD", $readMeHead);
-
+ file_put_contents("README.MD", $readMeHead);
