ae640eda05e068e6f35dccc733478a12c454cac7
tegic <teg1c@foxmail.com>
2021-10-08 17:06:32 +0800

    Code optimization

--- run.php
+++ run.php
@@ -21,24 +21,39 @@ $client = new Client([
     'allow_redirects' => true,
 ]);
 $response = $client->get($url, [
-        'headers'=>[
-                'User-Agent'=>'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
-            'Cookie'=>'SUB=_2AkMWPIzuf8NxqwJRmPgSxGrlZI1xwwvEieKgYH01JRMxHRl-yT8Xqn0HtRB6PbyiAY7W0wZkwFc1nXHJxUddZr9bpaPQ; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9W5p6Ee5xHahOckML_sA0l2c; _s_tentry=passport.weibo.com; Apache=6467906874370.302.1633682394000; SINAGLOBAL=6467906874370.302.1633682394000; ULV=1633682394006:1:1:1:6467906874370.302.1633682394000:; WBStorage=6ff1c79b|undefined'
-        ]
+    'headers' => [
+        'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+        'Cookie' => 'SUB=_2AkMWPIzuf8NxqwJRmPgSxGrlZI1xwwvEieKgYH01JRMxHRl-yT8Xqn0HtRB6PbyiAY7W0wZkwFc1nXHJxUddZr9bpaPQ; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9W5p6Ee5xHahOckML_sA0l2c; _s_tentry=passport.weibo.com; Apache=6467906874370.302.1633682394000; SINAGLOBAL=6467906874370.302.1633682394000; ULV=1633682394006:1:1:1:6467906874370.302.1633682394000:; WBStorage=6ff1c79b|undefined'
+    ]
 ]);
 $html = $response->getBody()->getContents();
 $table = QueryList::html($html)->find('table:eq(0)');
 $result = $table->find('tr:gt(1)')->map(function ($row) use (&$readMeHead) {
-    $regular = '@<a href="(\/weibo\?q=[^"]+)".*?>(.+)<\/a>@';
-    preg_match($regular, $row->find('td')->html(), $matches);
-    if (empty($matches)) {
+    $title = $row->find('td:eq(1)>a')->text();
+    $count = $row->find('td:eq(1)>span')->text();
+    $url = $row->find('td:eq(1)>a')->attr('href');
+    if (empty($title)) {
         return [];
     }
-    $readMeHead .= "1. [" . $matches[2] . "](https://s.weibo.com/" . $matches[1] . ")\n\n";
+    preg_match('@(\d+)@', $count, $matchs);
+
+    $readMeHead .= sprintf("1. [%s](https://s.weibo.com%s) %s 🔥 \n\n", $title, $url, getCountStr((int)($matchs[1] ?? '')));
     return [
-        'title' => $matches[2],
-        'url' => $matches[1],
+        'title' => $title,
+        'url' => $count,
     ];
 })->filter()->values();
 $content = $result->all();
- file_put_contents("README.MD", $readMeHead);
+file_put_contents("README.MD", $readMeHead);
+
+function getCountStr($num)
+{
+    $countUnit = ["", "K", "M", "B"];
+    $idx = 0;
+
+    while ($num / 1000 >= 1 && $idx < 3) {
+        $idx++;
+        $num = $num / 1000;
+    }
+    return sprintf("%.1f", $num) . $countUnit[$idx];
+}
